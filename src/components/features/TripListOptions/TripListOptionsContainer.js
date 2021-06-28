991fbee53cf1eb8521baf39a893138e88523dfb4
Jakub Jurga <>
2021-06-28 10:31:15 +0100

    Add filter redux, zadanie 17.5

--- src/components/features/TripListOptions/TripListOptionsContainer.js
+++ src/components/features/TripListOptions/TripListOptionsContainer.js
@@ -1,7 +1,7 @@
 import {connect} from 'react-redux';
 import TripListOptions from './TripListOptions';
 import {getAllTags} from '../../../redux/tagsRedux';
-import {getAllFilters, changeSearchPhrase} from '../../../redux/filtersRedux';
+import {getAllFilters, changeSearchPhrase, addTag, removeTag, changeFromDuration, changeToDuration} from '../../../redux/filtersRedux';
 
 const mapStateToProps = state => ({
   tags: getAllTags(state),
@@ -11,6 +11,12 @@ const mapStateToProps = state => ({
 const mapDispatchToProps = dispatch => ({
   changeSearchPhrase: phrase => dispatch(changeSearchPhrase(phrase)),
   // TODO - add more dispatchers for other filters
+
+  addTag: tags => dispatch(addTag(tags)),
+  removeTag: tags => dispatch(removeTag(tags)),
+  changeFromDuration: duration => dispatch(changeFromDuration(duration)),
+  changeToDuration: duration => dispatch(changeToDuration(duration)),
+
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(TripListOptions);
